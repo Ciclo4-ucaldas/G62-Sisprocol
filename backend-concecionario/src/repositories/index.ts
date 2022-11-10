6ec16baea39ab7209aae21ce96a8c7212ce3cc5c
Dario0915 <dario.bravosepulveda@gmail.com>
2022-11-10 08:22:09 -0500

    se crean repositorios de vendedor y venta

--- backend-concecionario/src/repositories/index.ts
+++ backend-concecionario/src/repositories/index.ts
@@ -1,3 +1,5 @@
 export * from './cliente.repository';
 export * from './factura.repository';
 export * from './usuario.repository';
+export * from './vendedor.repository';
+export * from './venta.repository';
