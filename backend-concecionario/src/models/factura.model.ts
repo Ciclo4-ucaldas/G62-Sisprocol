43587bf19cf7b5be438cdcac2a32e2450187a1ae
thunderthard <hars1302@gmail.com>
2022-11-09 22:12:24 -0500

    Creacion modelo factura

---
+++ backend-concecionario/src/models/factura.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Factura extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fecha: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  valor: number;
+
+
+  constructor(data?: Partial<Factura>) {
+    super(data);
+  }
+}
+
+export interface FacturaRelations {
+  // describe navigational properties here
+}
+
+export type FacturaWithRelations = Factura & FacturaRelations;
